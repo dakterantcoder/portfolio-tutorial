c373219dc462c53a2074f71e7f7c4e312cb7b0cf
Sherzod <dakterantcoder0001@gmail.com>
2023-11-26 22:27:48 +0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,44 @@
+/*"use strict"
+number=10;
+console.log(number); */
+// use strict bu qatiy rejim kalit so'zsiz elon qilib qilishni oldini olish uchun
+
+// number
+// let number=5.6; // Date Type=Number
+// console.log(8/0); //infinity ==> cheksizlik degani
+// console.log(-8/0); //infinity ==> -heksizlik degani
+// console.log("Sherzod"*8); //NaN ==> not a  number / mumkin emas
+
+// const telegram=prompt("What's your favourite telegram acount");
+// console.log("https://t.me/dakterantcoder01");
+
+// const channel=prompt("What's your favourite YouTuber")
+// console.log("https://youtube.com/");
+
+// const channel=prompt("What's your favourite YouTuber")
+// console.log("https://youtube.com/"+channel+"shorts");
+
+// const user="Sherzod"
+// const job="FrontEnd Devoloper"
+// console.log( `User name is ${user}. he is ${job}`);
+
+// let incr=10;
+// let decr=10;
+
+// console.log(++incr);
+// console.log(--decr);
+
+// console.log(++incr);
+// console.log(--decr);
+
+// console.log(incr);
+// console.log(decr);
+
+const age=+prompt("How old are yu?", "")
+
+if (age>18) {
+    console.log("Welcome");
+}
+else {
+    console.log("No entry");
+}
